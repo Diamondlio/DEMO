71a3b0d372a31124375013170c940e4aa0bb1fcc
QICL <www.chenglimg@qq.com>
2018-11-12 21:03:08 +0800

    第一次提交

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1{
+	
+	int a;
+
+	public void print(){
+	
+		System.out.println("hello world");
+	}
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Test Github for create repository
 
 我修改了readme文件
 即将提交该文件
+
+再次编辑了服务器上的README文件
